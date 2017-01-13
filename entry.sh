170c6f478577b73b0ebc1d86501e7091c9b186cb
Fabrizio Steiner <fas@bbtsoftware.ch>
2017-01-13 14:18:11 +0100

    Unmount the NFS mount on container stop.

--- entry.sh
+++ entry.sh
@@ -1,10 +1,19 @@
 #!/bin/dumb-init /bin/sh
 set -e
 
-mkdir -p "$MOUNTPOINT"
+umount_nfs () {
+  echo "Unmounting nfs..."
+  umount "$MOUNTPOINT"
+  exit
+}
 
+echo "Mounting nfs..."
+
+mkdir -p "$MOUNTPOINT"
 rpcbind -f &
 mount -t "$FSTYPE" -o "$MOUNT_OPTIONS" "$SERVER:$SHARE" "$MOUNTPOINT"
 mount | grep nfs
-#fg
+
+trap umount_nfs SIGHUP SIGINT SIGTERM
+
 while true; do sleep 1000; done
